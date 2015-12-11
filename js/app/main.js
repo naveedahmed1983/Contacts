011afb2761e9ba3ee033c525bb1bb80c1902fe51
naveedahmed1983 <naveedahmed1983@gmail.com>
2015-12-11 19:52:25 +0500

    changes in js

--- js/app/main.js
+++ js/app/main.js
@@ -1,44 +1,5 @@
 $(document).ready(function(){
 	
-	/*
 	
-	$("#btnlogin").hide();
-	
-	$('#pinCode').keypress(function (e) {
-		$("#err").text("");
- 		var key = e.which;
- 		if(key == 13)  // the enter key code
-  		{
-    		$("#btnCheck").click();
-    		return false;  
-  		}
-	});
-	
-	$("#btnCheck").click(function(){
-		//console.log("it worked");
-		//console.log($("#pinCode").val());
-		
-		var password = "un2014";
-    		if($("#pinCode").val() !== password) {
-				$("#pinCode").val("");
-        		$("#err").text("Access Denied! Please type the correct access code or contact Administrator for further assistance");
-    		}
-    		else {
-				
-				//$("#btnlogin").click();
-				console.log('correct code');
-				UNDirectoryApp.boot($('#TeamPageContents'));
-				  
-    		}
-	});
-	
-	
-	
-	
-$("#pinCode").click(function(){
-	$("#err").text("");
-	});
-		
-	*/
 	UNDirectoryApp.boot($('#TeamPageContents'));
 });
